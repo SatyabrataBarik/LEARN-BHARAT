0d6b0b69b206b74a9e8dfdd2e193d83653e8a678
satya <satyabrata.b@techouts.co.in>
2023-09-25 18:43:37 +0530

    ui-fix

--- src/components/HomePages/Header/page.tsx
+++ src/components/HomePages/Header/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 
 import Input from "@/components/Input/page";
+import { Size } from "@/utils/Size/page";
 import { useState } from "react";
 
 const Header = () => {
-  // const {fullHeight,fullWidth}=Size();
+  const {fullHeight,fullWidth}=Size();
   const [searchData, setSearchData] = useState("");
   const handlechange = (event:any) => {
     const {value}=event.target.value;
@@ -15,7 +16,7 @@ const Header = () => {
       style={{
         background: `url('https://t3.ftcdn.net/jpg/04/00/77/64/240_F_400776431_5JxdDYRr1mn9yISiUFMPcLtLp3zt6NA1.jpg') center center / cover no-repeat`, // Set the background image
         width: "100%",
-        height: "40rem",
+        height: "34rem",
         // display:'flex',
         // alignItems:'center'
       }}
