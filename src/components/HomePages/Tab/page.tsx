d8d72880865e20dcbbd9c642b756fa5fc0841ae3
satya <satyabrata.b@techouts.co.in>
2023-09-25 17:52:02 +0530

    ui-fix/tab

--- src/components/HomePages/Tab/page.tsx
+++ src/components/HomePages/Tab/page.tsx
@@ -1,39 +1,49 @@
-"use client"
+"use client";
 import { useState } from "react";
 
-const  data=[
-    {
-        
-        image:'',
-         icon:''
-    },
-    {
-        image:'',
-         icon:''
-    },
-    {
-        image:'',
-         icon:''
-    },
-]
+const data = [
+  {
+    image: "images/Vector (4).png",
+    icon: "",
+  },
+  {
+    image: "images/Group 1606.png",
+    icon: "",
+  },
+  {
+    image: "images/Vector (6).png",
+    icon: "",
+  },{
+    image:'images/Vector (8).png'
+  }
+];
 const Tab = () => {
-    const [clickedData,setClickData]=useState(0);
-    const handleClick=(index:number)=>{
-        setClickData(index);
-    }
-    console.log('clickedData', clickedData)
+  const [clickedData, setClickData] = useState(0);
+  const handleClick = (index: number) => {
+    setClickData(index);
+  };
+  console.log("clickedData", clickedData);
   return (
-    <div style={{display:'flex' ,justifyContent:'space-evenly'}}>
-    { data.map((allData,index)=>{
-       return <div onClick={()=>handleClick(index)}>
-               <img src={allData.image} alt="///"/>
-               <img src={allData.icon} alt="///"/>
-       </div>
-      
-      })
-    }
-       </div>
-  )
-}
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-evenly",
+        height: "5rem",
+        alignItems: "center",
+        backgroundColor:'#ffff',
+        boxShadow:'-1px 1px 3px -2px'
+      }}
+    >
+      {data.map((allData, index) => {
+        return (
+          <div onClick={() => handleClick(index)} style={{}}>
+            <img src={allData.image} alt="///"  style={{ height:index+1==data.length?14:24}}/>
+            
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
-export default Tab
+export default Tab;
